f7b1fddb4b68b9fbc027c0810eddc8c435c659df
Lee <1527778263@qq.com>
2025-02-25 23:54:25 +0800

    初次提交，上传资料与初始文件夹

---
+++ 后端web基础/05. 后端Web基础(数据库)/资料/05. DML语句需求/DML.sql
@@ -0,0 +1,28 @@
+
+-- DML : 数据操作语言
+-- DML : 插入数据 - insert
+-- 1. 为 emp 表的 username, password, name, gender, phone 字段插入值
+
+
+-- 2. 为 emp 表的 所有字段插入值
+
+
+-- 3. 批量为 emp 表的 username, password, name, gender, phone  字段插入数据
+
+
+
+-- DML : 更新数据 - update
+-- 1. 将 emp 表的ID为1员工 用户名更新为 'zhangsan', 姓名name字段更新为 '张三' 
+
+
+-- 2. 将 emp 表的所有员工的入职日期更新为 '2010-01-01'
+
+
+
+
+-- DML : 删除数据 - delete
+-- 1. 删除 emp 表中 ID为1的员工
+
+
+-- 2. 删除 emp 表中的所有员工
+
